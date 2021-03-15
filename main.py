997e08b2a8ffc0542fa1d1b5ef4e629a2cb45e53
denis_goncharov <info@muffs.ru>
2021-03-15 19:50:53 +0300

    WIP

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 """SIDNEY(COOKIE MONSTER) IMAGE MANAGER"""
 import os
 import io
+import magic
 from PIL import Image
+from flask_xcaptcha import XCaptcha
 from flask import (
     Flask,
     request,
@@ -19,16 +21,44 @@ from models import thumbnail
 application = Flask(__name__, template_folder="templates")
 application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tmp/app.db"
 application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+application.config["SIDNEY_COOKIE"] = 'http://localhost:5001'
+application.config["XCAPTCHA_SITE_KEY"] = ''
+application.config["XCAPTCHA_SECRET_KEY"] = ''
+application.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+application.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+
 db = SQLAlchemy(application)
+xcaptcha = XCaptcha(application)
+
+def post_verify():
+    if xcaptcha.verify():
+        return True
+    return False
+
+def get_image_mime(stream):
+    mime = magic.from_buffer(stream.read(1024), mime=True)
+    stream.seek(0)
+    return mime
+
+def image_to_object(image):
+    """convert image to Object"""
+    try:
+        file_object = io.BytesIO()
+        image.save(file_object)
+        file_object.seek(0)
+        return file_object
+    except:
+        print("Error in image_to_object()")
+        return abort(500)
 
 @application.route("/", methods=["GET", "POST"])
 def req_handler():
     #    """GET/POST requests handler"""
     #    try:
     if request.method == "GET":
-        thumbnail_link = request.args.get("thumbnail_link")
-        if thumbnail_link or thumbnail_link != "":
-            existing_thumbnail = thumbnail.query.filter(thumbnail.link == thumbnail_link).first()
+        thumbnail_id = request.args.get("thumbnail_id")
+        if thumbnail_id or thumbnail_id != "":
+            existing_thumbnail = thumbnail.query.filter(thumbnail.link == thumbnail_id).first()
             if existing_thumbnail:
                 view = request.args.get("view")
                 if view != "full":
@@ -37,14 +67,19 @@ def req_handler():
                     image.save(file_object, image.format)
                     file_object.seek(0)
                     return send_file(file_object, mimetype="*/*")
-                return make_response(render_template("thumbnail_info.html"), 200)        
+                return make_response(render_template("thumbnail_info.html"), 200)
     if request.method == "POST":
-        new_thumbnail = thumbnail(location="/home/alien/Projects/cookie/img/cat.jpg", link="012", size="56000", mtype="image/jpeg", hash="None")
-        db.session.add(new_thumbnail)
-        db.session.commit()
-        print(thumbnail.query.all())
-        return make_response(render_template("thumbnail_info.html"), 200)
-    return redirect("/index.html", code=302)
+#        if post_verify():
+            uploaded_images = request.files.getlist('images')
+            print (uploaded_images)
+            for uploaded_file in uploaded_images:
+                file_name = uploaded_file.filename
+                file_ext = os.path.splitext(file_name)[1]
+                file_mime_type = uploaded_file.content_type
+                if file_name != '' and file_ext in application.config['UPLOAD_EXTENSIONS']:
+                    return send_file(image_to_object(uploaded_file),mimetype="*/*")
+#                    return make_response(render_template("thumbnail_info.html"), 200)
+    return make_response(render_template("index.html"), 200)
 #    except:
 #        print("Error in req_handler()")
 #        return abort(404)
@@ -59,23 +94,15 @@ def method_forbidden(exception):
     """Method Not Allowed."""
     return jsonify(str(exception)), 405
 
-
 @application.errorhandler(404)
 def resource_not_found(exception):
     """Page not found."""
     return jsonify(str(exception)), 404
 
-
 @application.errorhandler(403)
 def resource_forbidden(exception):
     """Forbidden."""
     return jsonify(str(exception)), 403
-
-@application.route("/index.html")
-def default_index():
-    """Index page"""
-    return make_response(render_template("index.html"), 200)
-
 
 if __name__ == "__main__":
     application.run(threaded=True)
